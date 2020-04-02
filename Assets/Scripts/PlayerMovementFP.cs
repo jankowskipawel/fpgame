cc51f09cb1781bc76b37aeb7ca954a764334ee9e
jankowskipawel <paweljankowski246@gmail.com>
2020-04-02 23:26:04 +0200

    Add sprint and crouch, fix player height

--- Assets/Scripts/PlayerMovementFP.cs
+++ Assets/Scripts/PlayerMovementFP.cs
@@ -9,11 +9,15 @@ public class PlayerMovementFP : MonoBehaviour
     public float speed = 12f;
     public float gravity = -9.81f;
     public Transform groundCheck;
+    public Transform cellingCheck;
     public LayerMask groundMask;
-    public float jumpHeight = 3f;
+    public float jumpHeight = 2f;
+    public Camera cameraView;
     
     private Vector3 velocity;
     private bool isGrounded;
+    private bool isCellingAbove;
+    private bool crouchedRecently = false;
     
     private Animator _animator;
     private static readonly int IsRunning = Animator.StringToHash("isRunning");
@@ -29,6 +33,7 @@ public class PlayerMovementFP : MonoBehaviour
     void Update()
     {
         isGrounded = Physics.CheckSphere(groundCheck.position, 0.8f, groundMask);
+        isCellingAbove = Physics.CheckSphere(cellingCheck.position, 0.8f, groundMask);
 
         if (isGrounded && velocity.y < 0)
         {
@@ -49,7 +54,7 @@ public class PlayerMovementFP : MonoBehaviour
         }
         controller.Move(Time.deltaTime * speed * move);
 
-        if (Input.GetButton("Jump") && isGrounded)
+        if (Input.GetButtonDown("Jump") && isGrounded && !isCellingAbove)
         {
             velocity.y = (float)Math.Sqrt(jumpHeight * -2 * gravity);
         }
@@ -58,6 +63,35 @@ public class PlayerMovementFP : MonoBehaviour
         {
             velocity.y = 30;
             Debug.Log("CHEAT JUMP used.");
+        }
+        
+        //SPRINT
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            speed += 5;
+            Debug.Log("Sprint activated.");
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            speed -= 5;
+            Debug.Log("Sprint deactivated.");
+        }
+        
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            controller.height = 1;
+            controller.center = new Vector3(0, 0.5f, 0);
+            cameraView.transform.localPosition = new Vector3(0, 1f, 0);
+            crouchedRecently = true;
+        }
+        
+        if (!Input.GetKey(KeyCode.LeftControl) && !isCellingAbove && crouchedRecently)
+        {
+            controller.height = 1.75f;
+            controller.center = new Vector3(0, 0.9f, 0);
+            cameraView.transform.localPosition = new Vector3(0, 1.75f, 0);
+            crouchedRecently = false;
+            Debug.Log($"{Time.deltaTime}");
         }
         
         velocity.y += gravity * Time.deltaTime;
--- Assets/Scripts/ResourcePopup.cs
+++ Assets/Scripts/ResourcePopup.cs
@@ -7,43 +7,43 @@ using UnityEngine.UI;
 public class ResourcePopup : MonoBehaviour
 {
     private float t;
-        private float timer = 0;
-        private Vector3 startingPos;
-        private Vector3 endPos;
-        public TextMeshProUGUI text;
-        public RawImage imageObj;
-        private GameObject ui;
-        private float offset;
+    private float timer = 0;
+    private Vector3 startingPos;
+    private Vector3 endPos;
+    public TextMeshProUGUI text;
+    public RawImage imageObj;
+    private GameObject ui;
+    private float offset;
 
-        // Start is called before the first frame update
-        void Start()
-        {
-            offset = Random.Range(-10, 10);
-            ui = GameObject.FindGameObjectWithTag("UI");
-            transform.SetParent(ui.transform);
-            startingPos = new Vector3(Input.mousePosition.x+40, Input.mousePosition.y, 10);
-            endPos = new Vector3(startingPos.x+offset, startingPos.y + 50, startingPos.z);
-        }
-    
-        // Update is called once per frame
-        void Update()
-        {
-            timer += Time.deltaTime;
-            t += Time.deltaTime/0.2f;
-            transform.position = Vector3.Lerp(startingPos, endPos, t);
-            if (timer > 0.3f)
-            {
-                Destroy(gameObject);
-            }
-        }
-    
-        public void SetText(float amount)
-        {
-            text.text = $"+{amount}";
-        }
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = Random.Range(-10, 10);
+        ui = GameObject.FindGameObjectWithTag("UI");
+        transform.SetParent(ui.transform);
+        startingPos = new Vector3(Input.mousePosition.x+40, Input.mousePosition.y, 10);
+        endPos = new Vector3(startingPos.x+offset, startingPos.y + 50, startingPos.z);
+    }
 
-        public void SetIcon(Texture icon)
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        t += Time.deltaTime/0.2f;
+        transform.position = Vector3.Lerp(startingPos, endPos, t);
+        if (timer > 0.3f)
         {
-            imageObj.texture = icon;
+            Destroy(gameObject);
         }
+    }
+
+    public void SetText(float amount)
+    {
+        text.text = $"+{amount}";
+    }
+
+    public void SetIcon(Texture icon)
+    {
+        imageObj.texture = icon;
+    }
 }
